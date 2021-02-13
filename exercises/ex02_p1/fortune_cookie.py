634ec79fe74a7adbf0fcd4016df2f3b9014620fe
Your Name <you@example.com>
2021-02-12 21:45:07 -0500

    Ex2 Progress change 1

--- exercises/ex02_p1/fortune_cookie.py
+++ exercises/ex02_p1/fortune_cookie.py
@@ -2,17 +2,35 @@
 
 from random import randint
 
-__author__ = "YOUR 9-DIGIT PID"
+__author__ = "730324554"
 
 
 def main() -> None:
     """The entrypoint of the program, when run as a module."""
     print("Your fortune cookie says...")
-    # TODO 2: Print the result of calling your fortune_cookie function.
+    x: int = randint(0, 100)
+    print(my_fortune(randint(0, 100)))
     print("Now, go spread positive vibes!")
 
 
 # TODO 1: Define your fortune_cookie function here.
+def my_fortune(x: int) -> str:
+
+    if x < 50:
+        if x < 25: 
+            return("Great things are coming, you just have to be patient and wait.")
+        else:
+            return("Like a river flows into the sea... some things are just meant to be.")
+    else:
+        if x < 60:
+            return("Don't let your limitations overshadow your talents.")
+        else: 
+            if x < 75:
+                return("Happiness begins with facing life with a smile and a wink.")
+            else:
+                return("Keep your face to the sunshine and Syou wll never see shadows.")
+
+    
 
 
 # Python Idiom for "starting" the program when run as a module.
--- exercises/ex02_p1/tar_heels.py
+++ exercises/ex02_p1/tar_heels.py
@@ -1,15 +1,27 @@
 """Tar Heels exercise redux as a structured program."""
 
-__author__ = "YOUR 9-DIGIT PID"
+__author__ = "730342554"
 
 
 def main() -> None:
     """The entrypoint of the program, when run as a module."""
     choice: int = int(input("Enter an int: "))
     # TODO 2: Print the response of calling the tar_heels function here.
+    print(GDTBATH(choice))
 
 
 # TODO 1: Define the tar_heels function, and its logic, here.
+def GDTBATH(choice: int) -> str:
+    if ((choice % 2 == 0) and (choice % 7 == 0)):
+        print("TAR HEELS")
+    else:
+        if choice % 2 == 0:
+            print("TAR")
+        else:
+            if choice % 7 == 0:
+                print("HEELS")
+            else:
+                print("CAROLINA")
 
 
 if __name__ == "__main__":
